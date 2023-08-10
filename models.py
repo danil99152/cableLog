4b7096810bb8a076df7791b4415f1417dbde4d89
Danil <danil.komyshev777@gmail.com>
2023-08-10 04:48:42 +0300

    --[UPD] schema: separated aws to aws and cable line, [ADD] sqlalchemy and declarative models

---
+++ models.py
@@ -0,0 +1,78 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, DeclarativeBase
+
+# url to DB
+sqlite_database = "sqlite:///cables.db"
+
+# creation engine
+engine = create_engine(sqlite_database)
+
+
+# using creation basemodel before sqlalchemy 2.0.0
+# Base = declarative_base()
+
+# using creation basemodel after sqlalchemy 2.0.0
+class Base(DeclarativeBase):
+    pass
+
+
+class Server(Base):
+    __tablename__ = 'servers'
+
+    server_num = Column(Integer, primary_key=True, index=True)
+    server_name = Column(String(50), unique=True)
+
+
+class Department(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True, index=True)
+    department_name = Column(String(50), unique=True)
+
+
+class Employee(Base):
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True, index=True)
+    surname = Column(String(50))
+    name = Column(String(50))
+    middlename = Column(String(50))
+    department_id = Column(Integer, ForeignKey('departments.id'), index=True)
+
+    department = relationship(Department, backref='employees')
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.surname} {self.name} {self.middlename}'
+
+
+class AWS(Base):
+    __tablename__ = 'aws'
+
+    id = Column(Integer, primary_key=True, index=True)
+    pc_name = Column(String(50), unique=True)
+    ip = Column(String(15), unique=True)
+    mac = Column(String(17), unique=True)
+
+    employee_id = Column(Integer, ForeignKey('employees.id'), index=True)
+    employee = relationship(Employee, backref='cable_lines')
+
+
+class CableLine(Base):
+    __tablename__ = 'cable_lines'
+
+    id = Column(Integer, primary_key=True, index=True)
+    socket_num = Column(Integer)
+    socket_port = Column(Integer)
+    patchpanel_port = Column(Integer)
+    length = Column(Integer)
+
+    aws_id = Column(Integer, ForeignKey('aws.id'), index=True)
+    aws = relationship(AWS, backref='cable_lines')
+
+    server_id = Column(Integer, ForeignKey('servers.server_num'), index=True)
+    server = relationship(Server, backref='cable_lines')
+
+
+# creation tables if not exist
+Base.metadata.create_all(bind=engine)
